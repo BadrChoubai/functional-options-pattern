7f9bd49fd0889d125d977467a41e3e0dfc873565
Badr <choubai.badr@protonmail.com>
2024-03-08 19:31:22 -0700

    more changes

--- service.go
+++
@@ -1,9 +0,0 @@
-package main
-
-// Service struct encapsulates configuration related to connecting to an API
-type Service struct {
-	baseURL        string
-	clientId       string
-	clientSecret   string
-	healthcheckURL string
-}
